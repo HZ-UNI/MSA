e62f7b420a79dee987cd1362e61c085e9ebe08ab
Haichuan Zhang <hzha684@aucklanduni.ac.nz>
2022-08-08 11:56:44 +1200

    Call to api
    Basic api call implementation is done but the json file format is weird so the code that gets the img url needs to be worked on.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import axios from "axios";
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [digimonName, setDigimonName] = useState("");
+  const [digimonInfo, setDigimonInfo] = useState<undefined | any>(undefined);
+  const DIGIMON_BASE_URL = "https://digimon-api.vercel.app/api/digimon";
+
   return (
     <div>
-      <p>
-          
-      </p>
+      <h1>
+        Digimon Search
+      </h1>
+      
+      <div>
+        <label>Enter the name of the Digimon</label><br/>
+        <input 
+          type="text" 
+          id="digimon-name" 
+          name="digimon-name" 
+          onChange={(e) => setDigimonName(e.target.value)}/>
+          <br/>
+
+        <button onClick={search}>Search</button>
+      </div>
+
+      <p>You have entered {digimonName}</p>
+
+      {digimonInfo === undefined ? (
+        <p>Digimon not found</p>
+      ) : (
+        <div id="digimon-result">
+          <img src={digimonInfo.img} />
+        </div>
+      )}
     </div>
   );
+
+  function search(){
+    axios.get(DIGIMON_BASE_URL + "/name/" + digimonName).then((res) => {
+      setDigimonInfo(res.data);
+    });
+}
 }
 
 export default App;
