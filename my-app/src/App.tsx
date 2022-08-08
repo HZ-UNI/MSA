9b6f572aa3b9e96f33dcec1c819cfb19afd5496d
Haichuan Zhang <hzha684@aucklanduni.ac.nz>
2022-08-08 20:26:58 +1200

    changed api

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,45 +1,54 @@
 import axios from "axios";
 import { useState } from 'react';
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 import './App.css';
 
 function App() {
-  const [digimonName, setDigimonName] = useState("");
-  const [digimonInfo, setDigimonInfo] = useState<undefined | any>(undefined);
-  const DIGIMON_BASE_URL = "https://digimon-api.vercel.app/api/digimon";
+  const [characterName, setCharacterName] = useState("");
+  const [characterInfo, setCharacterInfo] = useState<undefined | any>(undefined);
+  const API_BASE_URL = "https://rickandmortyapi.com/api";
 
   return (
     <div>
       <h1>
-        Digimon Search
+        Character Search
       </h1>
       
       <div>
-        <label>Enter the name of the Digimon</label><br/>
+        <label>Enter the name of the Character</label><br/>
         <input 
           type="text" 
-          id="digimon-name" 
-          name="digimon-name" 
-          onChange={(e) => setDigimonName(e.target.value)}/>
+          id="character-name" 
+          name="character-name" 
+          onChange={(e) => setCharacterName(e.target.value)}/>
           <br/>
 
         <button onClick={search}>Search</button>
       </div>
 
-      <p>You have entered {digimonName}</p>
+      <p>You have entered {characterName}</p>
 
-      {digimonInfo === undefined ? (
-        <p>Digimon not found</p>
+      {characterInfo === undefined ? (
+        <p>Character not found</p>
       ) : (
-        <div id="digimon-result">
-          <img src={digimonInfo.img} />
+        <div id="result">
+          <img src={characterInfo.results[1].image} />
         </div>
       )}
     </div>
   );
 
   function search(){
-    axios.get(DIGIMON_BASE_URL + "/name/" + digimonName).then((res) => {
-      setDigimonInfo(res.data);
+    axios
+    .get(API_BASE_URL + "/character/?name=" + characterName)
+    .then((res) => {
+      setCharacterInfo(res.data);
+    })
+    .catch((err) => {
+      console.log("Pokemon not found");
+      setCharacterInfo(undefined);
     });
 }
 }
